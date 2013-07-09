6f90a3733e4b5aad6e8f0e362e4c6ac795a60bdc
DBC-Apprentice <dbc@dbc.com>
2013-07-08 17:09:04 -0700

    create migrations, models, routes, working v1

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,7 +7,49 @@ get '/' do
 end
 
 get '/:username' do
- puts params[username]
-#mkae an api call to grab 10 recent tweets from user name
-  erb :view_tweets
+
+  @user = User.find_or_create_by_username(params[:username])
+  p @user
+
+  if @user.tweets.empty?
+
+    user_tweets = Twitter.user_timeline(@user.username)
+
+    @recent_tweets = []
+
+    user_tweets.each do |tweet|
+      @recent_tweets << tweet.text
+    end
+
+    p @recent_tweets
+
+    new_tweets = Tweet.create(tweet: @recent_tweets, user_id: @user.id)
+    p new_tweets
+    @user.tweets << new_tweets
+    # User#fetch_tweets! should make an API call
+    # and populate the tweets table
+    #
+    # Future requests should read from the tweets table
+    # instead of making an API call
+    # @user.fetch_tweets!
+    erb :view_tweets
+  end
+
+  # @tweets = @user.tweets.limit(10)
+
 end
+
+
+
+# @saved_user = User.find_or_create_by_username(user)
+#   if @saved_user
+#     @all = []
+#     me = Twitter.user_timeline(@saved_user)
+#     me.each do |tweet|
+#       @all << tweet.text
+#     end
+#     @all
+#   else
+#   end
+
+#   erb :view_tweets
